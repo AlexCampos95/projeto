9d78c2ac4b19e6fefd06f8e97769e4434127a80a
Alex Campos <campos.z1995@gmail.com>
2020-08-22 15:12:04 -0300

    descriação do tópico

--- pp-users/app/Common/PubSubAbstraction.php
+++ pp-users/app/Common/PubSubAbstraction.php
@@ -2,7 +2,10 @@
 
 /** Classe fara a representação do PubSub,
  * o código executado nesse ponto seria assíncrono no mundo real
- * por conta da abstração com disparo Http será linear
+ * por conta da abstração com disparo Http será linear.
+ *
+ * O "disparo" do método run é a representação do tópico "user-created",
+ * onde irá executar a criação da carteira do mesmo.
  */
 class PubSubAbstraction
 {
