f9ad6e4538eb51afac260b073f6f9ce48ed1bdc5
Alex Campos <campos.z1995@gmail.com>
2020-08-22 15:07:43 -0300

    arquivo criado

---
+++ pp-users/app/Common/PubSubAbstraction.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Classe fara a representação do PubSub,
+ * o código executado nesse ponto seria assíncrono no mundo real
+ * por conta da abstração com disparo Http será linear
+ */
+class PubSubAbstraction
+{
+
+}
